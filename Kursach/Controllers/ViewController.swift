28f81443b3d2edbf10a8ce7b3800e593086e9826
Hleb Tsahoika <hlebtsahoika@icloud.com>
2023-12-12 03:22:24 +0300

    Change UI

--- Kursach/Controllers/ViewController.swift
+++ Kursach/Controllers/ViewController.swift
@@ -2,8 +2,9 @@ import UIKit
 import SnapKit
 
 private enum LayoutConstants {
-    static let messageTextFieldHeight: CGFloat = 40
-    static let messageTextFieldInsets: UIEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    static let defaultInsets: UIEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    static let navBarHeight: CGFloat = 44
+    static let textViewHeight: CGFloat = 150
 }
 
 final class ViewController: UIViewController {
@@ -11,21 +12,31 @@ final class ViewController: UIViewController {
     // MARK: - Properties
     private var viewModel : ApiViewModel
     
-    private lazy var messageTextField: UITextField = {
-        let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         
-        textField.placeholder = "Type any message..."
+        label.text = "Toxicity - a rude, disrespectful, or unreasonable comment that is likely to make someone leave a discusion\n\nType some text:"
         
-        textField.returnKeyType = .done
-        textField.layer.borderWidth = 0.5
-        textField.font = UIFont.systemFont(ofSize: 16)
-        textField.borderStyle = .roundedRect
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         
-        textField.delegate = self
-        textField.autocapitalizationType = .none
+        return label
+    }()
+    
+    private lazy var messageTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
         
-        return textField
+        textView.layer.cornerRadius = 15
+        textView.layer.borderWidth = 0.7
+        textView.backgroundColor = .white
+        textView.font = UIFont.systemFont(ofSize: 20)
+        textView.addDoneButton(title: "Done", target: self, selector: #selector(tapDone(sender:)))
+        
+        return textView
     }()
     
     private lazy var resultLabel: UILabel = {
@@ -34,22 +45,28 @@ final class ViewController: UIViewController {
         
         label.text = "result:"
         
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = .green
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = .black
         
         return label
+    }()
+    
+    private lazy var navBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        let navItem = UINavigationItem(title: "Text Toxicity analyzer")
+        navBar.setItems([navItem], animated: false)
+        navBar.setBackgroundImage(UIImage(), for: .default)
+        navBar.shadowImage = UIImage()
+        navBar.isTranslucent = true
+        return navBar
     }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .gray
         self.setup()
-        self.viewModel.bindEmployeeViewModelToController = {
-            let toxity = self.viewModel.empData?.attributeScores["TOXICITY"]?.summaryScore.value ?? -1
-            self.resultLabel.text = "result: "
-            + String(format: "%.3f", toxity)
-        }
+        self.setupLAyouts()
+        self.bindViews()
     }
     
     init(viewModel: ApiViewModel = ApiViewModel()) {
@@ -61,19 +78,46 @@ final class ViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Layouts setup
+    // MARK: - Setup
     private func setup() {
-        self.view.addSubview(messageTextField)
+        self.view.backgroundColor = .white
+    }
+    
+    private func bindViews() {
+        self.viewModel.bindEmployeeViewModelToController = {
+            let toxity = (self.viewModel.empData?.attributeScores["TOXICITY"]?.summaryScore.value ?? -1) * 100
+            self.resultLabel.text = "result: " + String(format: "%.2f", toxity) + "%"
+            
+            let color: UIColor = toxity < 25 ? .green : toxity < 50 ? .yellow : toxity < 75 ? .orange : toxity < 100 ? .red : .black
+            self.messageTextView.textColor = color
+        }
+    }
+    
+    private func setupLAyouts() {
+        self.view.addSubview(infoLabel)
+        self.view.addSubview(messageTextView)
         self.view.addSubview(resultLabel)
+        self.view.addSubview(navBar)
         
-        messageTextField.snp.makeConstraints { make in
-            make.top.left.right.equalTo(view.safeAreaLayoutGuide).inset(LayoutConstants.messageTextFieldInsets)
-            make.height.equalTo(LayoutConstants.messageTextFieldHeight)
+        navBar.snp.makeConstraints { make in
+            make.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(LayoutConstants.navBarHeight)
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(LayoutConstants.defaultInsets)
+            make.bottom.equalTo(messageTextView.snp.top).inset(-LayoutConstants.defaultInsets.bottom)
+        }
+        
+        messageTextView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.left.right.equalToSuperview().inset(LayoutConstants.defaultInsets)
+            make.height.equalTo(LayoutConstants.textViewHeight)
         }
         
         resultLabel.snp.makeConstraints { make in
-            make.left.right.bottom.equalTo(view.safeAreaLayoutGuide).inset(LayoutConstants.messageTextFieldInsets)
-            make.height.equalTo(LayoutConstants.messageTextFieldHeight)
+            make.top.equalTo(messageTextView.snp.bottom).offset(LayoutConstants.defaultInsets.top)
+            make.left.right.equalTo(view.safeAreaLayoutGuide).inset(LayoutConstants.defaultInsets)
         }
     }
     
@@ -81,18 +125,13 @@ final class ViewController: UIViewController {
     private func proccessText(text: String) {
         self.viewModel.callAPI(text: text)
     }
-}
-
-//MARK: - UITextFieldDelegate
-extension ViewController: UITextFieldDelegate {
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if let text = textField.text {
-            textField.resignFirstResponder()
+    // MARK: - Actions
+    @objc private func tapDone(sender: Any) {
+        let text = messageTextView.text ?? ""
+        self.view.endEditing(true)
+        if !text.isEmpty {
             self.proccessText(text: text)
-            return true
-        } else {
-            return false
         }
     }
 }
